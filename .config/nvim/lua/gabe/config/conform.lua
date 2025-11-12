2c88ce80606609666dedc31ae515fcce0ecb6c0b
Gabe Chantayan <garnet638@gmail.com>
2025-11-12 15:17:46 -0500

    more

--- .config/nvim/lua/gabe/config/conform.lua
+++ .config/nvim/lua/gabe/config/conform.lua
@@ -5,10 +5,11 @@ require("conform").setup({
 		python = { "isort", "black" },
 		-- You can customize some of the format options for the filetype (:help conform.format)
 		rust = { "rustfmt", lsp_format = "fallback" },
+		css = { "css_beautify" },
 		-- Conform will run the first available formatter
 		javascript = { "prettierd", "prettier", stop_after_first = true },
 		typescript = { "prettierd", "prettier" },
-		typescriptreact = { "prettierd", "prettier" },
+		typescriptreact = { "prettierd", "prettier", "rustywind" },
 	},
 	format_on_save = {
 		-- I recommend these options. See :help conform.format for details.
